b5a5f680b6479454f3c8f196ef07fd1c20442cd8
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-10-14 01:43:01 +0200

    feat: added 7 methods for the Collection interface

--- src/main/java/ayoub/collections/Collection.java
+++ src/main/java/ayoub/collections/Collection.java
@@ -1,5 +1,11 @@
 package ayoub.collections;
 
 public interface Collection<E> extends Iterable{
-
+    boolean add(E e);
+    boolean addAll(Collection<? extends E> c);
+    void clear(); // remove add elms of this collection
+    boolean contains(Object o);
+    boolean containsAll(Collection<?> c);
+    boolean equals(Object o);
+    int hashCode();
 }
